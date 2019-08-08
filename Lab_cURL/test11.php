92ec9e0c842c739914e8a7c387ef49010a9355f2
Daisy116 <52897966+Daisy116@users.noreply.github.com>
2019-08-08 17:38:42 +0800

    Add files via upload

---
+++ Lab_cURL/test11.php
@@ -0,0 +1,21 @@
+<?php
+header("content-type: text/html; charset=utf-8");
+
+// 1. 初始設定
+$ch = curl_init();
+
+// 2. 設定 / 調整參數
+curl_setopt($ch, CURLOPT_URL, "https://tw.appledaily.com/");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);    // 0代表不要HEADER，1代表要標頭
+
+// 3. 執行，取回 response 結果
+$output = curl_exec($ch);
+
+// 4. 關閉與釋放資源
+curl_close($ch);
+
+// echo $output;   // 會秀出網頁
+echo htmlspecialchars($output);  // 秀出html碼
+
+?>
